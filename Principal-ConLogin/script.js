962120224d770573e8be9675990b9124e1183f9d
AnthonyCod <englisghlife61@gmail.com>
2024-04-24 19:34:13 -0500

    usuario

--- Principal-ConLogin/script.js
+++
@@ -1,1 +0,0 @@
-const cartInfo = document.querySelector
---
+++ Usuario/javascript/productos.js
@@ -0,0 +1,59 @@
+const products = [
+    
+    {
+        id: 1,
+        img: "panPollo.png",
+        name: "Pan con Pollo",
+        price: "2.50",
+        button: "Comprar", 
+    },
+
+    {
+        id: 2,
+        img: "hamburguesa1.jpg",
+        name: " Hamburguesa",
+        price: "9.50",
+        button: "Comprar", 
+    },
+
+    {
+        id: 3,
+        img: "pizzas.jpg",
+        name: "Pizzas",
+        price: "4.00",
+        button: "Comprar", 
+    },
+
+    {
+        id: 4,
+        img: "broaster.png",
+        name: "Broaster",
+        price: "8.00",
+        button: "Comprar", 
+    },
+
+    {
+        id: 5,
+        img: "panPollo.png",
+        name: "Pan con Pollo",
+        price: "2.50",
+        button: "Comprar", 
+    },
+
+    {
+        id: 6,
+        img: "hamburguesa1.jpg",
+        name: " Hamburguesa",
+        price: "9.50",
+        button: "Comprar", 
+    },
+
+    {
+        id: 7,
+        img: "pizzas.jpg",
+        name: "Pizzas",
+        price: "4.00",
+        button: "Comprar", 
+    }
+
+]
