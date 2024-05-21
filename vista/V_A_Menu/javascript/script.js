b69ff3ff0b5315742c0d4bdffcafcc841a6dde8f
AnthonyCod <englishlife61@gmail.com>
2024-05-21 17:04:56 -0500

    carritoArreglado

--- vista/V_A_Menu/javascript/script.js
+++ vista/V_A_Menu/javascript/script.js
@@ -1,19 +1,21 @@
 const targetContainer = document.getElementById("targetContainer");
 
-function createProducts(producto){
-    producto.forEach(products => {
-        const newProduct = document.createElement("div");
-        newProduct.classList = "tarjetaProducto";
-        newProduct.innerHTML =  `
-        <img src="${products.img}">
-        <h2>${products.name}</h2>
-        <h3>${products.desp}</h3>
-        <h3 class="price">${products.price}</h3>
-        <button>${products.button}</button>
-    `   
-    targetContainer.appendChild(newProduct);
-    newProduct.getElementsByTagName("button")[0].addEventListener("click",()=> addProduct(products))
-    });
-}
+        function addProduct(product) {
+            console.log(`Producto agregado: ${product.name}`);
+        }
 
-createProducts(products);
+        function createProducts(producto) {
+            producto.forEach(products => {
+                const newProduct = document.createElement("div");
+                newProduct.classList.add("tarjetaProducto");
+                newProduct.innerHTML = `
+                    <img src="${products.img}" alt="${products.name}">
+                    <h2>${products.name}</h2>
+                    <h3>${products.desp}</h3>
+                    <h3 class="price">${"S/." + products.price}</h3>
+                    <button>${products.button}</button>
+                `;
+                targetContainer.appendChild(newProduct);
+                newProduct.querySelector("button").addEventListener("click", () => addProduct(products));
+            });
+        }
