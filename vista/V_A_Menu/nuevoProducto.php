75c5690c0ba5415865e6ec4fa133fc7366f86c6e
AnthonyCod <147573835+AnthonyCod@users.noreply.github.com>
2024-05-23 06:51:31 -0500

    Update nuevoProducto.php

--- vista/V_A_Menu/nuevoProducto.php
+++ vista/V_A_Menu/nuevoProducto.php
@@ -7,28 +7,46 @@ if (isset($_POST['saveProduct'])) {
     && strlen($_POST['productDescription']) >= 1 
     && strlen($_POST['productPrice']) >= 1){
 
-        $lista = trim($_POST['productList']);
-        $name = trim($_POST['productName']);
-        $description = trim($_POST['productDescription']);
-        $price = trim($_POST['productPrice']);
+        $lista = mysqli_real_escape_string($conexion, trim($_POST['productList']));
+        $name = mysqli_real_escape_string($conexion, trim($_POST['productName']));
+        $description = mysqli_real_escape_string($conexion, trim($_POST['productDescription']));
+        $price = mysqli_real_escape_string($conexion, trim($_POST['productPrice']));
 
-        $consulta1 = "select categoriaId from categoria where '$lista' = descripcion;";
+        $consulta1 = "SELECT categoriaId FROM categoria WHERE descripcion = '$lista' LIMIT 1;";
 
         $resultado1 = mysqli_query($conexion, $consulta1);
 
-        if($resultado1){
-            $prueba = mysqli_fetch_assoc($resultado1);  
+        if($resultado1 && mysqli_num_rows($resultado1) > 0){
+            $fila1 = mysqli_fetch_assoc($resultado1);
+            $categoriaId = $fila1['categoriaId'];
 
-            $consulta2 = "select RUC from establecimiento;";
+            $consulta2 = "SELECT RUC FROM establecimiento LIMIT 1;";
 
-            $resultado2 = mysqli_query($conexion, $consulta1);
+            $resultado2 = mysqli_query($conexion, $consulta2);
     
-            if($resultado2){
-                $prueba2 = mysqli_fetch_assoc($resultado2);  
-                $consulta = "INSERT INTO producto(categoriaId, RUC, nombreProducto, descripcionProducto,precio) VALUES ('$prueba','$prueba2','$name','$description','$price');";
-            }
-        }
-} 
-}
+            if($resultado2 && mysqli_num_rows($resultado2) > 0){
+                $fila2 = mysqli_fetch_assoc($resultado2);
+                $ruc = $fila2['RUC'];
 
+                $consulta3 = "INSERT INTO producto(categoriaId, RUC, nombreProducto, descripcionProducto, precio) 
+                VALUES ('$categoriaId', '$ruc', '$name', '$description', '$price');";
+
+                $resultado3 = mysqli_query($conexion, $consulta3);
+
+                if ($resultado3) {
+                    echo "Producto insertado correctamente.";
+                } else {
+                    echo "Error al insertar producto: " . mysqli_error($conexion);
+                }
+            } else {
+                echo "Error al obtener RUC: " . mysqli_error($conexion);
+            }
+        } else {
+            echo "Error al obtener categoriaId: " . mysqli_error($conexion);
+
+        }
+    } else {
+        echo "Todos los campos son requeridos.";
+}
+}
 ?>
