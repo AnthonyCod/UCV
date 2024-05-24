242eeaeedd5d1e7221a5fcdaea0b014b312a6af1
Clebi02 <ramoscleber13@hotmail.com>
2024-05-24 11:03:31 -0500

    MVC

--- controlador/prueba123.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "conexion.php";
-    $sql = $conexion->prepare("SELECT * FROM categoria");
-    $sql->execute();
-    $response = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
-    print_r($response);
-?>
