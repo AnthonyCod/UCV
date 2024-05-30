1e7e07b0c60a688b052f6e09ce0f8c7a76bacd5e
Clebi02 <cramosramosra@ucvvirtual.edu.pe>
2024-05-30 04:16:14 -0500

    Sin rutas

--- vista/V_V_Producto/index.php
+++ vista/V_V_Producto/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,42 +29,16 @@
                 <button type="button" class="modal__close fa-solid fa-xmark jsModalClose">X</button>
                 <div class="modal__info">
                     <div class="modal__header">
-                        <h2><i class="fas fa fa-shopping-cart"></i>Carrito de Compras</h2>
+                        <h2><i class="fas fa-shopping-cart"></i> Carrito de Compras</h2>
                     </div>
                     <div class="modal__body">
-                        <div class="modal__list">
-                            <div class="modal__item">
-                                <div class="modal__thumb">
-                                    <img src="../images/broaster.png" alt="Naranja">
-                                </div>
-                                <div class="modal__text-product">
-                                    <p>Naranja</p>
-                                    <p><strong>$9.00 / kg</strong></p>
-                                </div>
-                            </div>
-                            <div class="modal__item">
-                                <div class="modal__thumb">
-                                    <img src="../images/fresa.jpg" alt="Naranja">
-                                </div>
-                                <div class="modal__text-product">
-                                    <p>Manzana</p>
-                                    <p><strong>$5.00 / kg</strong></p>
-                                </div>
-                            </div>
+                        <!-- Aquí se añadirán los productos del carrito dinámicamente -->
+                        <div class="modal__list" id="carritoProductos">
+                            <!-- Productos cargados dinámicamente -->
                         </div>
                     </div>
-                    <div class="modal__footer">
-                        <div class="modal__list-price">
-                            <ul>
-                                <li>Subtotal: <strong>$14.00</strong></li>
-                                <li>Descuento: <strong>$0.00</strong></li>
-                            </ul>
-                            <h4 class="modal__total-cart">Total: $14.00</h4>
-                        </div>
-                        <div class="modal__btns">
-                            <a href="#" class="btn-primary">Comprar Ahora</a>
-                        </div>
-                    </div>
+                    <!-- Cambia el href a la página a la que deseas redirigir -->
+                    <a href="../V_A_Carrito/carrito.html" class="btn-primary">Comprar Ahora</a>
                 </div>
             </div>
         </div>
@@ -74,11 +48,6 @@
     <nav>
         <div class="foods">
             <ol>
-                <li><a href="#"><img src="../images/jugo.png"></a><span>Jugos</span></li>
-                <li><a href="#"><img src="../images/feijoada.png"></a><span>Criollo</span></li>
-                <li><a href="#"><img src="../images/pizza (2).png"></a><span>Pizzas</span></li>
-                <li><a href="#"><img src="../images/pierna-de-pollo (1).png"></a><span>Pollos</span></li>
-                <li><a href="#"><img src="../images/sandwich (1).png"></a><span>Sanguches</span></li>
                 <li><a href="#"><img src="../images/jugo.png"></a><span>Jugos</span></li>
                 <li><a href="#"><img src="../images/feijoada.png"></a><span>Criollo</span></li>
                 <li><a href="#"><img src="../images/pizza (2).png"></a><span>Pizzas</span></li>
@@ -104,7 +73,7 @@
         </div>
     </footer>
 
-    <script src="javascript/abrirCarrito.js"></script>
-    <script src="javascript/productos.js"></script> <!-- Incluyendo el script de productos -->
+    <script src="javascript/productos.js"></script>
+    <script src="javascript/abrirCarrito.js"></script>  <!-- Incluyendo el script de productos -->
 </body>
 </html>
