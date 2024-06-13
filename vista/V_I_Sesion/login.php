affe967b63de6704c34b3010f3a07e45d75d2352
KenithGP <kenithguanilo@gmail.com>
2024-06-12 19:26:58 -0500

    x

--- vista/V_I_Sesion/login.php
+++ vista/V_I_Sesion/login.php
@@ -1,48 +1,25 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
 <html lang="en">
-=======
-<html lang="es">
->>>>>>> 499a11045bf18ca573b82c49dce0fb002fdc7653
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="styles.css">
         <script src="https://kit.fontawesome.com/a91f4172e9.js" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
         <title>login</title>
         
-=======
-        <title>Iniciar sesión</title>
->>>>>>> 499a11045bf18ca573b82c49dce0fb002fdc7653
     </head>
     <body>
         <section>
             <div class="formulario">
                 <img src="../images/iconoPrincipal.png">
-<<<<<<< HEAD
-                <h1>Inicio de Sesion</h2>
-                    <h4 class="tipoUsuario">Empresa</h4>
-                    <h4>Correo electronico <i class="fa-regular fa-envelope"></i></h4>
-                    <input type="email" placeholder="  Correo Electronico...">
-                    <br><br>
-                    <h4>Contraseña <i class="fa-solid fa-lock"></i></h4>
-                    <input type="password" placeholder="  Contraseña...">
-                    <br><br>
-                    <a class="pass" href="#">Olvide la Contraseña</a>
-                    <br><br>
-                    <a class="boton" href="../V_A_Menu/index.php"> Ingresar </a>
-                    <br><br>
-<<<<<<<< HEAD:vista/V_I_Sesion/login.php
                     <h4>¿No tienes una cuenta? <a class="regi" href="../V_R_Cliente/register.html">Registrate</a></h4>
                     <br>
                     <h4>¿Eres empresa?</h4><a class="regi" href="../V_R_Establecimiento/register.html">Trabaja con Nosotros</a>
-========
+
                     <h4>¿No tienes una cuenta? <a class="regi" href="../V_R_Establecimiento/register.php">Registrate</a></h4>
                     <br>
->>>>>>>> 499a11045bf18ca573b82c49dce0fb002fdc7653:vista/V_I_Establecimiento/login.html
+
                 </div>
-=======
                 <h1>Inicio de Sesión</h1>
                 <form action="../../controlador/C_I_Sesion.php" method="POST" id='formInicio'>
                     <h4>Nombre de Usuario <i class="fa-regular fa-envelope"></i></h4>
@@ -61,7 +38,6 @@
                 <br>
                 <h4>¿Eres empresa?</h4><a class="regi" href="../RegistrarEmpresa/register.php">Trabaja con Nosotros</a>
             </div>
->>>>>>> 499a11045bf18ca573b82c49dce0fb002fdc7653
         </section>
     </body>
 </html>
