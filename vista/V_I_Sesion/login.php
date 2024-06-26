f1f080dccd7be73eee06cb7bbd812e0bcddbf8bb
KenithGP <kenithguanilo@gmail.com>
2024-06-26 13:12:50 -0500

    feat: login modification and settings

--- vista/V_I_Sesion/login.php
+++ vista/V_I_Sesion/login.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="styles.css">
         <script src="https://kit.fontawesome.com/a91f4172e9.js" crossorigin="anonymous"></script>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Iniciar sesión</title>
     </head>
@@ -13,17 +14,18 @@
                 <img src="../images/iconoPrincipal.png">
                 <h1>Inicio de Sesión</h1>
                 <form action="../../controlador/C_I_Sesion.php" method="POST" id='formInicio'>
-                    <h4>Nombre de Usuario <i class="fa-regular fa-envelope"></i></h4>
-                    <input type="text" name="nombreUsuario" placeholder="NombreUsuario..." required>
+                 
+                    <label>
+                        <i class='bx bx-envelope'></i>
+                        <input type="email" placeholder="Correo Electrónico" name="correo" id="correo" required>
+                    </label>
+                    <label>
+                        <i class='bx bx-lock-alt'></i>
+                        <input type="password" placeholder="Contraseña" name="contraseña" id="contraseña" required>
+                    </label>
+                    <input type="submit" value="Iniciarr">
                     <br><br>
-                    <h4>Contraseña <i class="fa-solid fa-lock"></i></h4>
-                    <input type="password" name="contraseña" placeholder="Contraseña..." required>
-                    <br><br>
-                    
                     <a class="pass" href="#">Olvidé la Contraseña</a>
-                    <br><br>
-                    <button type="submit" class="boton">Ingresar</button>
-                    <br><br>
                 </form>
                 <h4>¿No tienes una cuenta? <a class="regi" href="../V_R_Cliente/register.php">Regístrate</a></h4>
                 <br>
