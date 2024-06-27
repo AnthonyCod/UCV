917fbe0283ebba4da997e49ab333c61a46243751
facundo <facga20145@gmail.com>
2024-06-26 21:34:01 -0500

    boton carrito funciona

--- vista/V_V_Producto/javascript/abrirCarrito.js
+++ vista/V_V_Producto/javascript/abrirCarrito.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const addToCartButton = document.querySelector('[data-btn-action="add-btn-cart"]');
+    const addToCartButton = document.getElementById('addToCartButton');
     const closeModalButton = document.querySelector('.jsModalClose');
     const modal = document.getElementById('jsModalCarrito');
 
-    addToCartButton.addEventListener('click', () => {
+    addToCartButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
         modal.classList.add('active');
     });
 
@@ -16,4 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
             modal.classList.remove('active');
         }
     });
+<<<<<<< HEAD
+=======
+
+    // Asegurarse de que el enlace "Comprar Ahora" redirija correctamente
+    const btnComprarAhora = document.getElementById('comprarAhora');
+    if (btnComprarAhora) {
+        btnComprarAhora.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenir el comportamiento por defecto
+            window.location.href = btnComprarAhora.getAttribute('href');
+        });
+    } else {
+        console.error('No se encontró el botón "Comprar Ahora".');
+    }
+>>>>>>> 91f8095 (boton carrito funciona)
 });
